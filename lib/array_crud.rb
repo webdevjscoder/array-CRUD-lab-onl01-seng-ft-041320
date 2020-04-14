b3c79fc6ddd9ab3e950c72d92de7a652877af7fb
Juan Cobian <cobianjuan19@gmail.com>
2020-04-14 18:16:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,9 +8,10 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Mike", "Timmy", "Rebecca"]
+  array << "Tom"
 end
 
-add_element_to_end_of_array
+add_element_to_end_of_array(array, element)
 
 def add_element_to_start_of_array(array, element)
   
